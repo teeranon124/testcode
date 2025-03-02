6947bc9bb28a8d34600943cb5cbdf798463d8f21
teeranon thongsidam <teeranonn124@gmail.com>
2025-03-02 15:49:48 +0700

    add code cat and mouse

---
+++ cat_mouse.py
@@ -0,0 +1,10 @@
+def cat_and_mouse(x, y, z):
+    distance_a = abs(x - z)
+    distance_b = abs(y - z)
+
+    if distance_a < distance_b:
+        return "Cat A"
+    elif distance_b < distance_a:
+        return "Cat B"
+    else:
+        return "Mouse C"
