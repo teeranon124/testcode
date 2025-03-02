8515ff7f0c261c38d505f9365bbea9c496486bda
teeranon thongsidam <teeranonn124@gmail.com>
2025-03-02 16:46:01 +0700

    edit len grid

--- homework5.py
+++ homework5.py
@@ -3,7 +3,7 @@ def gridChallenge(grid):
         grid[i] = "".join(sorted(grid[i]))
 
     for col in range(len(grid[0])):
-        for row in range(1, n):
+        for row in range(1, len(grid)):
             if grid[row][col] < grid[row - 1][col]:
                 return "NO"
 
